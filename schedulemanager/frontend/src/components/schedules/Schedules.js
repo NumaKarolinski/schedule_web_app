05d59dbde6c6d4fdd8751ca758bae0a7af440fe3
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2020-10-05 13:47:36 -0400

    Implementing React

---
+++ schedulemanager/frontend/src/components/schedules/Schedules.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Schedules extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Schedules List</h1>
+      </div>
+    );
+  }
+}
+
+export default Schedules;
