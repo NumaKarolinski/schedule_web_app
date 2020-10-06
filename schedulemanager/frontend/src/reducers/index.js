7d1092721913ada0f7e9743665a26dc184012481
Numa Karolinski <numa.karolinski@mail.mcgill.ca>
2020-10-06 13:29:28 -0400

    Auth State & Private Routes

--- schedulemanager/frontend/src/reducers/index.js
+++ schedulemanager/frontend/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import schedules from "./schedules";
 import errors from "./errors";
 import messages from "./messages";
+import auth from "./auth";
 
 export default combineReducers({
   schedules,
   errors,
   messages,
+  auth,
 });
